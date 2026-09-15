3294c293d77a9dbf53a12aa9a47c17a0f885ffe5
Irfanul Haque <121337802+Irfanul017@users.noreply.github.com>
2026-09-15 09:47:20 +0530

    Time: 22 ms (20.36%), Space: 45.6 MB (77.26%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,72 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        
+        List<List<Integer>> quadrantList = new ArrayList<>();
+
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 3; i++) {
+
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                // Skip duplicate j
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    long sum = (long) nums[i]
+                             + nums[j]
+                             + nums[left]
+                             + nums[right];
+
+                    if (sum == target) {
+
+                        quadrantList.add(Arrays.asList(
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        ));
+
+                        // Skip duplicates on left
+                        while (left < right &&
+                               nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+
+                        // Skip duplicates on right
+                        while (left < right &&
+                               nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+
+                        // Move both pointers
+                        left++;
+                        right--;
+                    }
+
+                    else if (sum > target) {
+                        right--;
+                    }
+
+                    else {
+                        left++;
+                    }
+                }
+            }
+        }
+
+        return quadrantList;
+    }
+}
